b0ef58fd11630257b7faca84d9ba03a561a8f97f
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2021-11-22 11:04:22 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,7 +1,7 @@
 #ifndef _my_mat_h  
 # define _my_mat_h
-void func1(int mat [10][10]);
-int func2(int mat[10][10]);
-int func3(int mat[10][10]);
+void foo1(int mat [10][10]);
+int foo2(int mat[10][10]);
+int foo3(int mat[10][10]);
 
 #endif
